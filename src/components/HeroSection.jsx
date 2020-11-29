58802279a0a4bef3c06e53246286d60b43c1c18f
Manuel Canadas <nanocanadas@gmail.com>
2020-11-30 12:37:58 +1300

    Video diplayed and cards component created

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,7 +3,7 @@ import '../App.css';
 import {Button} from './Button';
 import './HeroSection.css'
 
-funtion HeroSection() {
+function HeroSection() {
     return (
         <div className='hero-container'>
             <video src='/videos/video-2.mp4' autoPlay loop muted />
