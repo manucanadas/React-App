cf757156f1c72f50a134624d7e4f82d332f8528f
Manuel Canadas <nanocanadas@gmail.com>
2020-12-01 16:53:51 +1300

    first migrations

--- migrations/20201201162700_users.js
+++ migrations/20201201162700_users.js
@@ -1,8 +1,11 @@
-
-exports.up = function(knex) {
+exports.up = function (knex) {
+    return knex.schema.createTable('users', table => {
+      table.increments('id')
+      table.string('name')
+      table.string('email')
+    })
+  }
   
-};
-
-exports.down = function(knex) {
-  
-};
+  exports.down = function (knex) {
+    return knex.schema.dropTable('users')
+  }
--- migrations/20201201162901_products_services.js
+++ migrations/20201201162901_products_services.js
@@ -1,8 +1,13 @@
 
-exports.up = function(knex) {
+exports.up = function (knex) {
+    return knex.schema.createTable('products_services', table => {
+      table.increments('id')
+      table.integer('service_id')
+      table.integer('order_id')
+      table.integer('quantity')
+    })
+  }
   
-};
-
-exports.down = function(knex) {
-  
-};
+  exports.down = function (knex) {
+    return knex.schema.dropTable('products_services')
+  }
